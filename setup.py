db514df0774c7f5e4e00262e3861898bac79d7c7
Priyatosh Anand <priyatoshanand92@gmail.com>
2024-02-16 12:47:21 +0530

    final

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'priyatosh anand',
+    author_email= 'priyatoshanand92@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
